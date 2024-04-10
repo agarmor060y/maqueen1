8abdb2a739238722814905171598991d0dca0d93
agarmor060y <153504876+agarmor060y@users.noreply.github.com>
2024-04-10 13:20:05 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
+        maqueen.motorStop(maqueen.Motors.All)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    }
+    basic.showNumber(maqueen.Ultrasonic(PingUnit.Centimeters))
+})
